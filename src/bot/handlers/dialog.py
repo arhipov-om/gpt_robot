096914552cf1f4cb91c8882a0efb4889637923ac
Arhipov.OM <arhipov.om@mail.ru>
2025-12-13 23:05:24 +0300

    feat(llm-integration): добавлена интеграция с LangChain и OpenAI для обработки диалогов с памятью Добавлена поддержка генерации ответов от LLM с учетом истории разговора:
    Новый модуль infrastructure/llm.py с функцией get_llm_text и цепочкой ConversationChain.
    Новый репозиторий RedisMemoryRepository для хранения истории чатов в Redis.
    Инициализация ChatOpenAI и репозитория в main.py с передачей в Dispatcher.
    Обновлен обработчик process_text_message для использования LLM.
    Добавлены зависимости langchain-community и langchain-openai в pyproject.toml.

--- src/bot/handlers/dialog.py
+++ src/bot/handlers/dialog.py
@@ -1,10 +1,24 @@
 from aiogram import F, Router
 from aiogram.types import Message
+from langchain_core.language_models import BaseChatModel
+
+from infrastructure.llm import get_llm_text
+from infrastructure.repository import RedisMemoryRepository
 
 router = Router()
 
 
 @router.message(F.text)
-async def process_text_message(message: Message) -> None:
+async def process_text_message(
+    message: Message,
+    llm: BaseChatModel,
+    memory_repository: RedisMemoryRepository,
+) -> None:
     """Обрабатывает входящие текстовые сообщения."""
-    await message.answer(text=message.text)
+    llm_answer = await get_llm_text(
+        llm=llm,
+        memory_repository=memory_repository,
+        input_query=message.text,
+        chat_id=message.chat.id,
+    )
+    await message.answer(text=llm_answer)
---
+++ src/infrastructure/repository.py
@@ -0,0 +1,40 @@
+"""Модуль для работы с хранилищем Redis."""
+
+from langchain_community.chat_message_histories import RedisChatMessageHistory
+
+
+class RedisMemoryRepository:
+    """Репозиторий для работы с памятью в Redis."""
+
+    def __init__(self, redis_url: str) -> None:
+        """Инициализирует репозиторий.
+
+        :param redis_url: ЮРЛ для подключения Redis.
+        """
+        self.redis_url = redis_url
+
+    async def get_chat_history(self, chat_id: int) -> RedisChatMessageHistory:
+        """Возвращает историю чата из Redis.
+
+        :param chat_id: ID чата.
+        :return: Объект истории чата.
+        """
+        return RedisChatMessageHistory(session_id=str(chat_id), url=self.redis_url)
+
+    async def add_user_message(self, chat_id: int, message: str) -> None:
+        """Добавляет сообщение пользователя в историю.
+
+        :param chat_id: ID чата.
+        :param message: Сообщение пользователя.
+        """
+        history = await self.get_chat_history(chat_id)
+        history.add_user_message(message)
+
+    async def add_ai_message(self, chat_id: int, message: str) -> None:
+        """Добавляет сообщение от AI в историю.
+
+        :param chat_id: ID чата.
+        :param message: Сообщение от AI.
+        """
+        history = await self.get_chat_history(chat_id)
+        history.add_ai_message(message)
